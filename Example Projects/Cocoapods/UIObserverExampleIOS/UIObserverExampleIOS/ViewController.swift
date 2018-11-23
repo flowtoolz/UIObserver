c99db5e51218575e3ef18dbb46d90a6500f8d689
Sebastian <contact@flowtoolz.com>
2018-11-23 16:12:29 +0100

    Demonstrate in example project how to use UIButton

--- Example Projects/Cocoapods/UIObserverExampleIOS/UIObserverExampleIOS/ViewController.swift
+++ Example Projects/Cocoapods/UIObserverExampleIOS/UIObserverExampleIOS/ViewController.swift
@@ -1,11 +1,64 @@
 import UIKit
+import GetLaid
 import UIObserver
+import SwiftObserver
 
-class ViewController: UIViewController {
-
-    override func viewDidLoad() {
+class ViewController: UIViewController, Observer
+{
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
-        view.backgroundColor = UIColor.green
+        
+        exampleButton.constrainToParentExcludingTop()
+        exampleButton.constrainHeight(to: 100)
+        
+        titleUpdateButton.constrainToParentExcludingBottom()
+        titleUpdateButton.constrainHeight(to: 100)
+        
+        observe(exampleButton)
+        {
+            event in
+            
+            switch event
+            {
+            case .touchDown: print("touchDown")
+            case .touchUpInside: print("touchUpInside")
+            case .touchUpOutside: print("touchUpOutside")
+            case .touchCancel: print("touchCancel")
+            default: print("control event value: \(event.rawValue)")
+            }
+        }
     }
+    
+    private lazy var titleUpdateButton: UIButton =
+    {
+        let btn = view.addForAutoLayout(UIButton())
+        
+        btn.addTarget(self,
+                      action: #selector(modifyObservableTitle),
+                      for: .touchUpInside)
+        btn.backgroundColor = UIColor.red
+        btn.setTitle("Modify Other Button Title", for: .normal)
+        
+        return btn
+    }()
+    
+    @objc private func modifyObservableTitle()
+    {
+        observableTitle <- (observableTitle.value ?? "") + "_*"
+    }
+    
+    private lazy var exampleButton: UIButton =
+    {
+        let btn = view.addForAutoLayout(UIButton())
+        
+        btn.backgroundColor = UIColor.red
+        btn.setupObservability()
+        btn.present(observableTitle)
+        
+        return btn
+    }()
+    
+    private let observableTitle = Var("Tap Me and Watch the Console")
 }
 
